ecc4fd93581d554177311c4598e69bcefaaf8de1
Igor Adelino <igoradelino@gmail.com>
2023-06-02 15:34:06 -0300

    migrations fixed

--- db/migrations/20230530160431_add-session-id-to-diet.ts
+++ db/migrations/20230530160431_add-session-id-to-diet.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('user', (table) =>{
-    table.uuid('session_id').after('id').index()
+    table.uuid('session_id').unique().after('id').index()
   }).alterTable('diet', (table) => {
     table.uuid('meal_session_id').references('session_id').inTable('user')
   })
